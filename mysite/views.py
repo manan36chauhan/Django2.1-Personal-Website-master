381c40ef64f47cc8f684b1f64b8752c394c1fe14
manan36chauhan <63107421+manan36chauhan@users.noreply.github.com>
2022-06-28 12:56:35 +0530

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,47 @@
+import json
+
+import requests
+from django.shortcuts import render
+
+from .models import Contact
+
+
+def index(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('fname')
+        lastname = request.POST.get('lname')
+
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
+        return render(request, 'mysite/index.html', context)
+    else:
+        firstname = 'Attreya'
+        lastname = 'Bhatt'
+
+        r = requests.get('http://api.icndb.com/jokes/random?firstName=' + firstname + '&lastName=' + lastname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+
+        context = {'joker': joke}
+        return render(request, 'mysite/index.html', context)
+
+
+def portfolio(request):
+    return render(request, 'mysite/portfolio.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        email_r = request.POST.get('email')
+        subject_r = request.POST.get('subject')
+        message_r = request.POST.get('message')
+
+        c = Contact(email=email_r, subject=subject_r, message=message_r)
+        c.save()
+
+        return render(request, 'mysite/thank.html')
+    else:
+        return render(request, 'mysite/contact.html')
